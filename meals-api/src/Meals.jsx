3df7ea6005922a293b5239b91ad3f9e62acfa4b3
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-05-14 22:46:07 +0530

    meals-api done

--- meals-api/src/Meals.jsx
+++ meals-api/src/Meals.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./styles.css";
 
 const Meals = () => {
-  return <div>Meals</div>;
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood")
+      .then((res) => {
+        setMeals(res.data.meals);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const mealslist = meals.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section key={Math.random()} className="card">
+        <img src={strMealThumb} />
+        <section className="content">
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+
+  return <div className="items-container">{mealslist}</div>;
 };
 
 export default Meals;
