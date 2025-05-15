332c914efcfd384c877381344c9dd1e5c1fb3425
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-05-15 13:12:29 +0530

    color-toggler - deployed

--- color-toggler/vite.config.js
+++ color-toggler/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-projects-basic/todo-app/",
+  base: "/react-projects-basic/color-toggler/",
   plugins: [react()],
 });
