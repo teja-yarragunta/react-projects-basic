bfc19b45c0dd65e50adf6e83e55d94305e54d405
teja-yarragunta <mail.yarragunta.teja@gmail.com>
2025-05-24 21:39:39 +0530

    form-validation - done

--- form-validation/src/App.jsx
+++ form-validation/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import FormValidation from "./FormValidation";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <FormValidation />
+    </div>
+  );
 };
 
 export default App;
